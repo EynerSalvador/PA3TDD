6678bcd35da237fcc989d54dd9048dacacda4c97
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-26 22:33:44 -0500

    Update Project.test.js

--- assets/js/tests/Project.test.js
+++ assets/js/tests/Project.test.js
@@ -14,7 +14,7 @@ if (!window.fetch) {
 }
 
 // Importa la clase Project si estás usando módulos ES6
-import Project from '../js/Project.js';  // Ajusta la ruta según corresponda
+import Project from '../Project.js';  // Ajusta la ruta según corresponda
 
 describe('Modelo Project', () => {
   
